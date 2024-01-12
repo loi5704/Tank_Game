2ce091e2ce8610699084d9c78a295714023b3e9a
loi5704 <queolewlew@gmail.com>
2024-01-12 21:14:47 +0700

    DrawMap
    Hello

--- TankGame2/Assets/Script/Bullet.cs
+++ TankGame2/Assets/Script/Bullet.cs
@@ -53,25 +53,11 @@ public class Bullet : MonoBehaviour
     {
         Debug.Log("Collidered" + collision.name);
         OnHit?.Invoke();
-        bool check = true;
-
         var damagable = collision.GetComponent<Damagable>();
         if (damagable != null)
         {
-            check = false;
             damagable.Hit(bulletData.damage);
         }
-
-        if(check)
-        {
-            var damagableTank = collision.GetComponent<DamagableTank>();
-            if (damagableTank != null)
-            {
-                damagableTank.Hit(bulletData.damage);
-            }
-        }
-
-
 
         DisableObject();
     }
